8ab4cb17b0a85bdfdb76577c614629c6f64e9621
Andrea Virgillito <71944456+PythonUser-ux@users.noreply.github.com>
2023-12-26 20:38:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.use(function (err, req, res, next) {
 
 // instead of "on" because we are listening for this event one time, 'open' is the event we are listening for, it is emitted once the the mongoDB is successfully connected
 mongoose.connection.once('connected', () => {
-    console.log('Connected to MongoDB');
+    // console.log('Connected to MongoDB');
     console.log(`server running on port ${PORT}`);
     app.listen(PORT, () => console.log(`server running on port ${PORT}`));
 });
